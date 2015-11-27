dd24a021f3cfbf1c8d49abb683d01a7afc4c6628
Alex Abate <abate@email.arizona.edu>
2015-11-27 11:50:46 -0700

    can now scrape all event names and urls from given date range

--- tmp_scrape.py
+++ tmp_scrape.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
-# see this
+# see this:
 # http://nycdatascience.com/nyc-concert-data-web-scraping-with-python/
 
 # function to scrape info from a given link on site
@@ -88,11 +88,15 @@ def scrape(link):
     return [venue, minprice, maxprice, neighborhood, address_full, time]
     
 
-link = "http://www.villagevoice.com/event/john-pizzarelli-and-jessica-molaskey-7532546"
-
+#link = "http://www.villagevoice.com/event/john-pizzarelli-and-jessica-molaskey-7532546"
 #scrape(link)
 
+#url = "http://www.tucsonweekly.com/tucson/EventSearch?narrowByDate=2015-11-27"
+#text = requests.get(url).text
+#print text
 
+
+# The code below searches FIRST PAGE only of today's listings
 numDays = 1
 for j in range(0, numDays):
 
@@ -102,32 +106,56 @@ for j in range(0, numDays):
     print "DATE:"
     print dateString
     
-    # parse webpage with BeautifulSoup
-    url = 'http://www.tucsonweekly.com/tucson/EventSearch?narrowByDate=' + dateString
-    text = requests.get(url).text  # get raw html
-    soup = BeautifulSoup(text)     # input it into BeautifulSoup to parse it
+    baseUrl = 'http://www.tucsonweekly.com/tucson/EventSearch?narrowByDate='
+    
+    # while loop to crawl over pages
+    npageMax = 1000 # maximum possible number of pages of events: a huge number, and the code should break 
+                    # before this
+
+
+    # base URL for this date
+    url = baseUrl + dateString
+    
+    for pageNum in xrange(1, npageMax):
+        print "On page =", pageNum
+    
+        # parse webpage with BeautifulSoup
+        if (pageNum>1):
+            url = baseUrl + dateString + "&page=" + str(pageNum)
+        
+        text = requests.get(url).text  # get raw html
+        soup = BeautifulSoup(text)     # input it into BeautifulSoup to parse it
 
     
-    # get whole event div
-    eventTags = soup.find_all('div', 'EventListing clearfix')[1:]
-    print type(eventTags)
-    
-    for i, event in enumerate(eventTags):
-        #if (i>12):
-        #    print event
+        # get all "div" tags that corresond to a single event
+        eventTags = soup.find_all('div', 'EventListing clearfix')
+        print "There are", len(eventTags), "events on this page"
+        
+        # break out of loop over pages if no events found
+        if (len(eventTags)<1):
+            break
+            
+        # loop over each event and attempt to extract the html link for the page of the event
+        for i, event in enumerate(eventTags):
 
-        print "EVENT START", i
-        maybe = event.find_all('a')
-        print len(maybe), "a's", type(maybe)
-        isFound = False
-        for j,mb in enumerate(maybe):
-            print j#, mb, type(mb)
-            if (mb.has_attr('href') and (not isFound) and (not mb.has_attr('class')) ):
-                print mb.get_text()    # this is the event name!
-                print mb.attrs['href'] # this is the event webpage!
-                isFound = True
-        print "\n"
-    print len(eventTags), "events"
-    
-#text = requests.get(link).text
+            print "EVENT", i ,"START:"
+        
+            # find all the anchor ('a') tags within the 'div' tag for the evetn
+            anchor_tags = event.find_all('a')
+        
+            # iterate over each anchor tag looking for the FIRST one that has a 'href' attribute
+            # but does NOT have a 'class' attribute
+            # NOTE: due to poor structure of Tucson Weekly event div's this may be the only way
+            #       however it may not be robust/work for everything?
+            isFound = False
+            for j,mb in enumerate(anchor_tags):
+                if (mb.has_attr('href') and (not isFound) and (not mb.has_attr('class')) ):
+                    event_name = mb.get_text().strip() # this should be the event name! (type unicode)
+                    event_link = mb.attrs['href']      # this should be the event webpage! (type string)
+                    print "Event name =", event_name
+                    print "Event link =", event_link
+                    isFound = True
+            print "\n"
+        
+
 
